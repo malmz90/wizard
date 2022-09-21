cd34aecb03cfbb550ec60637b5fa8e7ad87716b8
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-09-21 20:18:35 +0200

    img slides

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { data } from './imageData'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
+const slideLeft = () => {
+  let slider = document.getElementById('slider')
+  slider.scrollLeft = slider.scrollLeft - 500
+}
+const slideRight = () => {
+  let slider = document.getElementById('slider')
+  slider.scrollLeft = slider.scrollLeft + 500
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-red-400'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App bg-gray-200 mt-40">
+      <h1 className="font-semibold"> UTVALDA PRODUKTER</h1>
+      <div className="relative flex items-center">
+        <MdChevronLeft
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+        />
+        <div
+          id="slider"
+          className="flex flex-row w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
         >
-          Learn React
-        </a>
-      </header>
+          {data &&
+            data.map((item) => (
+              <div>
+                <img
+                  className="w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
+                  src={item.img}
+                  alt="/"
+                />
+                <p>{item.model}</p>
+              </div>
+            ))}
+        </div>
+
+        <MdChevronRight
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
